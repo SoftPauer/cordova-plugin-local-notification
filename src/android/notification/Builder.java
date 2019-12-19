0e313ed728b27a63c9d6fd42559b9b655340288e
Samuel Higgs <sam.higgs@softpauer.com>
2019-12-19 12:25:45 +0000

    missing semi-colon

--- src/android/notification/Builder.java
+++ src/android/notification/Builder.java
@@ -29,7 +29,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationCompat.MessagingStyle.Message;
-import androidx.media.app.NotificationCompat.MediaStyle
+import androidx.media.app.NotificationCompat.MediaStyle;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
